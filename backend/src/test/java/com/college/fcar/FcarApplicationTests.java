2ee63aec3ed57266d861343873ec09cf4f07b82d
Anand <anandramdaskale@gmail.com>
2026-08-04 20:14:14 +0530

    spring initializr

---
+++ backend/src/test/java/com/college/fcar/FcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.fcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
